540561e8b6aecdde5a20d5bc1387f26b9690bbfe
prorakeshraut <35736338+prorakeshraut@users.noreply.github.com>
2022-07-04 16:56:21 +0545

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE test(
-    id INT NOT NULL UNIQUE,
-    name VARCHAR(50) NOT NULL,
-    age INT NOT NULL CHECK(age>=18),
-    phone INT(15) NOT NULL,
-    city VARCHAR(20) NOT NULL DEFAULT 'Kathmandu'
+    id INT,
+    name VARCHAR(50),
+    age INT,
+    phone INT(15),
+    city VARCHAR(20)
 );
