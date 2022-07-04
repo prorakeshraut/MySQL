08793f081b07b930e612072b44c10e70f06c7e69
prorakeshraut <35736338+prorakeshraut@users.noreply.github.com>
2022-07-04 17:49:19 +0545

    Add files via upload

---
+++ between_not-between.sql
@@ -0,0 +1,9 @@
+SELECT * FROM test
+WHERE age BETWEEN 18 AND 20;/*It return the value greater than 18 and less than 20*/
+WHERE age NOT BETWEEN 18 AND 20;/*IT return the value that is not between 18 and 20 */
+
+WHERE id BETWEEN 2 AND 5; /*It return the rows from  2 to 5 */
+WHERE id NOT BETWEEN 2 AND 5; /*It return the rows except from 2 to 5; */
+ 
+WHERE name BETWEEN "a" AND "k"; /*It returns the name start from a to k */
+WHERE name NOT BETWEEN "a" AND "k"; /*It returns the name starts from l to z except a to k */
