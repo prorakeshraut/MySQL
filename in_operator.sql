cadcfe12d9740a456532f1e8d78aca4291c9ceab
prorakeshraut <35736338+prorakeshraut@users.noreply.github.com>
2022-07-04 17:29:52 +0545

    Add files via upload

---
+++ in_operator.sql
@@ -0,0 +1,4 @@
+SELECT * FROM test
+WHERE age IN(18,21); /*It will give the result having the age 18 and 21 */
+WHERE age NOT IN(18,21); /*It will give the result which dont have the age 18 and 21
+
